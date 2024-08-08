9affdba8c98666691a1af73a04ad937d1edff8fa
Abhishek Tiwari <82721930+coder0501@users.noreply.github.com>
2024-08-08 12:02:54 +0530

    Update app.js

--- mern-lead-app/server/app.js
+++ mern-lead-app/server/app.js
@@ -23,28 +23,28 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 app.use(cors());
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect('mongodb://localhost:27017/mern-lead-app');
 
 app.use('/users', usersRouter);
 app.use('/leads', leadsRouter);
 
-// app.use((req, res, next) => {
-//   next(createError(404));
-// });
+app.use((req, res, next) => {
+  next(createError(404));
+});
 
-// app.use((err, req, res, next) => {
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   res.status(err.status || 500);
-//   res.json({ error: err.message });
-// });
+  res.status(err.status || 500);
+  res.json({ error: err.message });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
